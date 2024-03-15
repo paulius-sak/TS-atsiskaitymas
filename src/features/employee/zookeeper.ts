21f8478bca10c85483d723f9b72f718d403827e8
paulius-sak <aspauliusas@gmail.com>
2024-03-15 07:53:10 +0200

    little changes

--- src/features/employee/zookeeper.ts
+++ src/features/employee/zookeeper.ts
@@ -1,33 +1,33 @@
-import {Employee} from "./Employee"
+// import {Employee} from "./Employee"
 
-class Zookeeper extends Employee {
-    protected feedLog: { animal: string; time: Date }[];
+// class Zookeeper extends Employee {
+//     protected feedLog: { animal: string; time: Date }[];
 
-    constructor() {
-        super("");
-        this.feedLog = [];
-    }
+//     constructor() {
+//         super("");
+//         this.feedLog = [];
+//     }
 
-    enterZoo(): void {
-        this.isEmployeeAtZoo = true;
-    }
+//     enterZoo(): void {
+//         this.isEmployeeAtZoo = true;
+//     }
 
-    leaveZoo(): void {
-        this.isEmployeeAtZoo = false;
-    }
+//     leaveZoo(): void {
+//         this.isEmployeeAtZoo = false;
+//     }
 
-    takeSafetyTrainings(): void {
+//     takeSafetyTrainings(): void {
         
-        this.safetyTrainingCompletionDate = new Date();
-    }
+//         this.safetyTrainingCompletionDate = new Date();
+//     }
 
-    feedAnimal(animal: string): void {
+//     feedAnimal(animal: string): void {
         
-        this.feedLog.push({ animal, time: new Date() });
-    }
+//         this.feedLog.push({ animal, time: new Date() });
+//     }
 
-    getFeedLog(): { animal: string; time: Date }[] {
-        return this.feedLog;
-    }
-}
+//     getFeedLog(): { animal: string; time: Date }[] {
+//         return this.feedLog;
+//     }
+// }
 
